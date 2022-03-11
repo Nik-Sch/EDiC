ee2900fdc736760dcca823d9c687f7c19ef55fb6
Nik-Sch <mail@niklas-schelten.de>
2022-03-11 14:19:42 +0100

    template for thesis

---
+++ thesis/docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker run -ti -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --net=host \
+  -v $(pwd):/home/developer/project -w /home/developer/project --rm n1ksch/latexbloat $@
