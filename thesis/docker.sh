80adc332aeb7303cfc44f414a1004921e5dfa570
Nik-Sch <mail@niklas-schelten.de>
2022-04-28 19:38:46 +0200

    started with design adaptations

--- thesis/docker.sh
+++ thesis/docker.sh
@@ -2,4 +2,4 @@
 docker run -ti -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   --net=host \
-  -v $(pwd):/home/developer/project -w /home/developer/project --rm n1ksch/latexbloat $@
+  -v $(pwd):/home/developer/project -w /home/developer/project --rm n1ksch/latexbloat:22 $@
